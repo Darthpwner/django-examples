46d8998388759b8f0aa54da32c169bad7bedeb26
Matthew Lin <matthew_lin@apple.com>
2016-07-27 15:10:36 -0700

    Play around with this

--- ember_showcaser/quotes/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from quotes import models
-
-admin.site.register(models.Quote)
--- ember_showcaser/quotes/views.py
+++
@@ -1,7 +0,0 @@
-from django.views import generic
-
-
-class MainPageView(generic.TemplateView):
-    template_name = 'index.html'
-
-main_page = MainPageView.as_view()

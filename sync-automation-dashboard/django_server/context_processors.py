43c45b48ff53b4d8e8353c0e60427f53ea3b3732
Matthew Lin <matthew_lin@apple.com>
2016-07-28 11:24:30 -0700

    Final name change

--- sync-automation-dashboard/django_server/context_processors.py
+++
@@ -1,5 +0,0 @@
-# from django_server.models import Category
-
-
-# def categories(request):
-#     return {'categories': Category.objects.all()}
--- sync-automation-dashboard/ember_client/static/application/app.js
+++
@@ -1,2 +0,0 @@
-window.Dashboard = Ember.Application.create();
-

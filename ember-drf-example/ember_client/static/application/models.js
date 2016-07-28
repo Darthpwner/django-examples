d6e2cc0c850297630d700e32ebc5ce63c25fe783
Matthew Lin <matthew_lin@apple.com>
2016-07-28 00:34:47 -0700

    Fix more bugs please

--- ember-drf-example/ember_client/static/application/models.js
+++ ember-drf-example/ember_client/static/application/models.js
@@ -1,16 +1,17 @@
-(function(Blog, $, undefined ) {
-    Blog.ApplicationAdapter = DS.DjangoRESTAdapter.extend({
+(function(Dashboard, $, undefined ) {
+    Dashboard.ApplicationAdapter = DS.DjangoRESTAdapter.extend({
         namespace: "api"
     });
-    Blog.Category = DS.Model.extend({
+    Dashboard.Category = DS.Model.extend({
         name: DS.attr('string'),
         slug: DS.attr('string')
     });
-    Blog.Post = DS.Model.extend({
+    Dashboard.Post = DS.Model.extend({
         title: DS.attr('string'),
         slug: DS.attr('string'),
         text: DS.attr('string'),
         category: DS.belongsTo('category', {async: true}),
         posted_date: DS.attr('date')
     });
-}(window.Blog, jQuery));
+    
+}(window.Dashboard, jQuery));
